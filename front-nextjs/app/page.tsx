bed635d09f21f85a5b4de7282b511ac5837ee8fc
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2025-11-16 13:03:38 -0500

    (chore): fix2

--- front-nextjs/app/page.tsx
+++ front-nextjs/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import apiClient from '@/lib/api';
+import apiClient from '../lib/api';
 
 export default function Home() {
   const [data, setData] = useState(null);
